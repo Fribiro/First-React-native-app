41f39be92c38edd70e50b036bcde4c835f2310fc
fribiro <festusribiro@gmail.com>
2024-01-04 23:49:08 +0300

    Landing component

---
+++ app/screens/intro.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Intro = () => {
+  return (
+    <View style={styles.container}>
+      <Text></Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+})
+export default Intro;
