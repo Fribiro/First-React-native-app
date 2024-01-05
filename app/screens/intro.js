c4abb490a808bef8ab84252d2ec226068b29a5e1
fribiro <festusribiro@gmail.com>
2024-01-05 22:55:04 +0300

    Entry component accepting  user input for later user in user page

--- app/screens/intro.js
+++ app/screens/intro.js
@@ -1,15 +1,61 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StatusBar, StyleSheet, Dimensions, TextInput } from 'react-native';
+import colors from '../misc/colors';
 
-const Intro = () => {
+const Intro = ({ onFinish }) => {
+    const [name, setName] = useState('');
+    const handleOnChangeText = text => setName(text);
+
+    const handleSubmit = async () => {
+      const user = { name: name };
+      await AsyncStorage.setItem('user', JSON.stringify(user));
+      if (onFinish) onFinish();
+    };
   return (
-    <View style={styles.container}>
-      <Text></Text>
-    </View>
+    <>
+      <StatusBar hidden />
+      <View style={styles.container}>
+        <Text style={styles.inputTitle}>Enter Your Name to Continue</Text>
+        <TextInput
+          value={name}
+          onChangeText={handleOnChangeText}
+          placeholder='Enter Name'
+          style={styles.textInput}
+        />
+        {name.trim().length >= 3 ? (
+          <RoundIconBtn antIconName='arrowright' onPress={handleSubmit} />
+        ) : null}
+      </View>
+    </>
   );
 };
 
-const styles = StyleSheet.create({
+//extract width of the device
+const width = Dimensions.get('window').width - 50;
 
-})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textInput: {
+    borderWidth: 2,
+    borderColor: colors.PRIMARY,
+    color: colors.PRIMARY,
+    width,
+    height: 50,
+    borderRadius: 10,
+    paddingLeft: 15,
+    fontSize: 20,
+    marginBottom: 15,
+  },
+  inputTitle: {
+    alignSelf: 'flex-start',
+    paddingLeft: 25,
+    marginBottom: 5,
+    opacity: 0.5,
+  },
+});
+
 export default Intro;
